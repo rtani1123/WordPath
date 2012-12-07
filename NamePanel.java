2170c0943e05be7f599b55cbdbc71e20e4caa6c0
Ryan Tani <ryan.tani1123@gmail.com>
2012-12-07 00:28:14 -0800

    Adjusted sizes and fixed some logic dealing with entering of names.

--- NamePanel.java
+++ NamePanel.java
@@ -3,11 +3,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class NamePanel extends JPanel implements ActionListener {
+public class NamePanel extends JPanel implements ActionListener, KeyListener {
 
 	WordPathClient wpc;
 	JPanel appPanel = new JPanel();
 	JPanel nameEntryPanel = new JPanel();
+	JLabel statusLabel = new JLabel("");
 	JTextField nameEntryField;
 	JButton nameAcceptButton;
 	
@@ -24,14 +25,25 @@ public class NamePanel extends JPanel implements ActionListener {
 		gbc.gridx = 3;
 		gbc.gridy = 3;
 		add(appPanel, gbc);
-		appPanel.add(new JLabel("Name"));
+		appPanel.add(new JLabel("Enter Name: "));
 		appPanel.add(nameEntryPanel);
 		
 		nameEntryPanel.setLayout(new BoxLayout(nameEntryPanel, BoxLayout.X_AXIS));
 		nameEntryPanel.add(nameEntryField);
 		nameEntryPanel.add(nameAcceptButton);
+		
+		gbc.gridy = 4;
+		appPanel.add(statusLabel, gbc);
 		nameAcceptButton.addActionListener(this);
+		nameEntryField.addKeyListener(this);
 	}
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			nameAcceptButton.doClick();
+		}
+	}
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 	public void actionPerformed(ActionEvent ae) {
 		if(ae.getSource() == nameAcceptButton) {
 			wpc.checkNames(nameEntryField.getText());
